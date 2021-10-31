bdba426aa5c847c34685328e035637e1fafc0b0a
zhaobin <1920746358@qq.com>
2021-11-01 02:15:23 +0800

    chore(架构层): git Submit to limit。

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feature', 'fix', 'revert', 'perf', 'refactor', 'optimize', 'style', 'docs', 'test', 'build']],
+    'type-enum': [
+      2,
+      'always',
+      ['feature', 'fix', 'revert', 'perf', 'refactor', 'optimize', 'style', 'docs', 'chore', 'test', 'build']
+    ],
     'type-empty': [2, 'never'],
     'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
-    'subject-full-stop': [0, 'never'],
-    'header-max-length': [0, 'always', 100]
+    'subject-full-stop': [2, 'never'],
+    'header-max-length': [2, 'always', 100]
   }
 }
