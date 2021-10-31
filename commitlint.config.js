643e3f6211fc7ca0a42dd7dcef5292d7baf9d81c
zhaobin <1920746358@qq.com>
2021-11-01 02:16:25 +0800

    chore(架构层): git Submit to limit.

--- commitlint.config.js
+++ commitlint.config.js
@@ -10,6 +10,6 @@ module.exports = {
     'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never'],
-    'header-max-length': [2, 'always', 100]
+    'header-max-length': [2, 'always', 99]
   }
 }
